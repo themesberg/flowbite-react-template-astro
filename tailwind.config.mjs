b202641009d1389628892eabcd29572f8a57967b
SutuSebastian <sebiitv@gmail.com>
2024-04-04 16:06:37 +0300

    chore: bump `flowbite-react`

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,13 @@
+import flowbite from "flowbite-react/tailwind";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [flowbite.plugin()],
 };
